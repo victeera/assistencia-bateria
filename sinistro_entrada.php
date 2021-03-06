90a809b17c7cdcdc05ef3c531100b1f1401d70f7
Victor Alexandre <alexanvictor@gmail.com>
2021-03-05 20:35:56 -0400

    Criando tela para geração de PDF

---
+++ sinistro_entrada.php
@@ -0,0 +1,55 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link type="text/css" rel="stylesheet" href="css/estilo.css">
+    <title>Controle de Asistencia</title>
+</head>
+<body>
+<div class="corpo">
+<div class="container">
+    <fieldset class="fieldset">
+        <div class="logo">
+        <div class="div-logo">LOGO</div>
+        <div class="div-text">
+            The county was affected by the Miao rebellion of 1736-36. The county is home to many Dong people.
+
+            On 1385, the abolition of Liping long lawsuit, built five Wei command division, military duct, administer 15 2 villages, 14 executive secretary. Ming Yongle eleven years (1413), the abolition of thinking, to appease our, suppose the Liping mansion in Liping village officer corps, attached to the Guizhou receives announcement administration to enable our, Tan Xi jurisdiction, eight boat, the ancient state, Cao drop Hou Fei, hung, drawn and ferro permanent from, Xishan Yang Dong (waste) seven executive our, implemented within civil military divide and conquer; Wu Kai Wei jurisdiction, Tun, Huguang.
+        </div>
+
+    </div></fieldset>
+    <fieldset class="fieldset">
+    <div class="topo">
+
+        <div><h1>Controle de Asistência</h1></div>
+            <div><h1>000716</h1></div>
+    </div>
+    </fieldset>
+    <fieldset class="fieldset">
+    <div class="center">
+        <div>1</div>
+        <div>2</div>
+    </div>
+    </fieldset>
+    <fieldset class="fieldset">
+    <div class="center">
+        <div>1</div>
+        <div>2</div>
+    </div>
+    </fieldset>
+    <fieldset class="fieldset">
+    <div class="end">
+        <div>1</div>
+        <div>2</div>
+    </div>
+        <fieldset class="fieldset">
+</div>
+</div>
+</body>
+</html>
